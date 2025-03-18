0319f213ea0358730d59ebfd2fcec0b92667c0ff
FURY\subha <subha6555@gmail.com>
2025-03-18 23:50:31 +0530

    Add project files.

---
+++ PortVault/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PortVault
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ PortVault/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PortVault
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
